173faad572f42e44c46e2ab43ad4b0bc68f16c0c
bonowg <bonowg@gmail.com>
2018-02-11 19:45:23 +0100

    kurs

--- app/views.py
+++ app/views.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 20*"hELLO world !!!"
+    user = {'nickname': 'Wojciech'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
