7ad987192cfee1f27d057be4e958a894ebf92424
bonowg <bonowg@gmail.com>
2018-02-11 20:01:11 +0100

    kurs

--- app/views.py
+++ app/views.py
@@ -4,7 +4,24 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Wojo The Man'}  # fake user
-    return render_template('index.html',
+    user = {'nickname': 'Miguel'}  # fake user
+
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Wojtek',},
+            'body': 'Pierwszy wpis od Wojteka'
+        }
+    ]
+
+    return render_template("index.html",
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
