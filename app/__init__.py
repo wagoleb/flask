a3ba96f635ff72ea65812ed865ca6424e8a5b024
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-02-12 13:47:31 +0100

    kurs

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
+import os
+
+from config import basedir
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
