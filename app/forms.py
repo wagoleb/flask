bfeb1af6369cd9218cf202100e356955bce7e5f4
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-02-12 15:13:17 +0100

    kurs

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,12 @@ from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired, Length
 
+class ErrorsMessages():
+    dataRequired = 'To pole nie moze byc puste'
+
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
+    openid = StringField('openid', validators=[DataRequired(message = ErrorsMessages.dataRequired)])
     remember_me = BooleanField('remember_me', default=False)
 
 class Testowy(Form):
-    name = StringField('name', validators=[DataRequired(), Length(min=2, max=20)])
+    name = StringField('name', validators=[DataRequired(message = ErrorsMessages.dataRequired), Length(min=2, max=20)])
