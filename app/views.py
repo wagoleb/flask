9d20402332a94eb8fb49bd608a70a8fab95673fa
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-02-12 09:43:17 +0100

    kurs

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,4 +22,12 @@ def index():
         }
     ]
 
-    return render_template("index.html", title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="{}", remember_me={}'.format(form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
