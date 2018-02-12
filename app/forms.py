bbe5c719fa135a0ff0dc64db0d68a9a8b525800c
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-02-12 14:55:37 +0100

    kurs

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class Testowy(Form):
+    name = StringField('name', validators=[DataRequired(), Length(min=2, max=20)])
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app, db, lm, oid
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from .forms import LoginForm
+from .forms import LoginForm, Testowy
 from .models import User
 
 @app.route('/')
@@ -20,7 +20,7 @@ def index():
         },
         {
             'author': {'nickname': 'Wojtek',},
-            'body': 'Pierwszy wpis od Wojteka'
+            'body': 'Pierwszy wpis od Wojtka'
         }
     ]
 
@@ -33,6 +33,14 @@ def login():
         flash('Login requested for OpenID="{}", remember_me={}'.format(form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
+
+@app.route('/test', methods=['GET', 'POST'])
+def testowy():
+    form = Testowy()
+    if form.validate_on_submit():
+        flash('Name: {}'.format(form.name.data))
+        return redirect('/index')
+    return render_template('testowy.html', title='Testing Form', form=form)
 
 @lm.user_loader
 def load_user(id):
