4660402cc1bbcb69768340c8de9933f49f3e99d5
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-02-13 13:51:09 +0100

    login

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_openid import OpenID
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-# lm = LoginManager()
-# lm.init_app(app)
-# oid = OpenID(app, os.path.join(basedir, 'tmp'))
+login = LoginManager(app)
 
 from app import models, views
 
