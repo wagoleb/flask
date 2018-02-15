a1f386eafa8254935b288442eb7a9164b5866a95
bonowg <bonowg@gmail.com>
2018-02-15 20:47:56 +0100

    pokazywanie postow

--- app/views.py
+++ app/views.py
@@ -82,8 +82,5 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = Post.query.filter_by(author=user).all()
     return render_template('user.html', user=user, posts=posts)
