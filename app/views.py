2e305b01189eda0a60cb4cdba3537703e3a15d22
bonowg <bonowg@gmail.com>
2018-02-15 21:54:43 +0100

    edycja profilu

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from app import app, db
 from datetime import datetime
 from flask import render_template, flash, redirect, session, url_for, request, g, url_for
 from flask_login import login_user, logout_user, current_user, login_required
-from forms import LoginForm, Testowy, Login, RegistrationForm
+from forms import LoginForm, Testowy, Login, RegistrationForm, EditProfileForm
 from models import User, Post
 from tzlocal import get_localzone
 from werkzeug.urls import url_parse
@@ -99,3 +99,18 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
